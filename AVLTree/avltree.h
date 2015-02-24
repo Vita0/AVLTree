62186e824acbde87a75968c083476b89d3b53b4e
Виталя <Vita0@users.noreply.github.com>
2015-02-25 00:09:23 +0400

    Creating Files
    avltree.h - signatures of methods

---
+++ AVLTree/avltree.h
@@ -0,0 +1,16 @@
+class AVLTree {
+public:
+    AVLTree(); // конструктор по умолчанию
+    ~AVLTree(); // деструктор
+    void add(int number); // добавление числа в дерево
+    void del(int number); // удаление числа из дерева
+    // проверка наличия числа, результат true если есть
+    bool contains(int number) const;
+    int size() const; // выдат количество чисел в дереве
+    // Добавить все числа из tree в это дерево
+    void addAll(const AVLTree& tree);
+    // Удалить все числа из tree из этого дерева
+    void delAll(const AVLTree& tree);
+    void clear(); // очистить дерево
+    bool empty(); // вернуть true если пустое
+};
