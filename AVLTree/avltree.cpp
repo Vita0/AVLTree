43bc19cb78ebb59f188d9f2379c782d4fc1b1cc6
Виталя <Vita0@users.noreply.github.com>
2015-02-25 01:05:51 +0400

    sth

--- AVLTree/avltree.cpp
+++ AVLTree/avltree.cpp
@@ -1,2 +1,19 @@
 #include "avltree.h"
 
+AVLTree::AVLTree()
+    :value(0),
+    avlt_left(nullptr),
+    avlt_right(nullptr),
+    height(1)
+{}
+
+AVLTree::AVLTree(AVLTree &avlt)
+    :value(avlt.value)
+{
+    value
+}
+
+//int AVLTree::getValue()
+//{
+//    return value;
+//}
